ab4d305108914600c86f5218d9f1759b0c143f1e
Jerod Bury <jbury@kochava.com>
2018-06-20 15:53:50 -0700

    Add initial react front end - video 6

---
+++ client/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "globals": {
+        "document": true
+    },
+    rules: {
+        "react/jsx-filename-extension": "off",
+    }
+};
---
+++ client/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+import registerServiceWorker from './registerServiceWorker';
+import Routes from './routes';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8081/graphql',
+});
+
+const App = (
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>
+);
+
+ReactDOM.render(App, document.getElementById('root'));
+registerServiceWorker();
